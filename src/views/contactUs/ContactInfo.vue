ca2ff4385fac54da505c808881879e6c9503da49
wangxu <w_angxu@163.com>
2021-03-20 21:06:00 +0800

    finish contact info

--- src/views/contactUs/ContactInfo.vue
+++ src/views/contactUs/ContactInfo.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="content_container">
     <p class="title">{{$t('menu.contactInfo')}}</p>
+    <div class="item_wrap">
+      <p class="item">
+        <span class="label">{{$t('contactUs.companyName.label')}}：</span>
+        <span class="value">{{$t('contactUs.companyName.value')}}</span>
+      </p>
+      <p class="item">
+        <span class="label">{{$t('contactUs.address.label')}}：</span>
+        <span class="value">{{$t('contactUs.address.value')}}</span>
+      </p>
+      <p class="item">
+        <span class="label">{{$t('contactUs.zipCode.label')}}：</span>
+        <span class="value">{{$t('contactUs.zipCode.value')}}</span>
+      </p>
+      <p class="item">
+        <span class="label">{{$t('contactUs.phone.label')}}：</span>
+        <span class="value">{{$t('contactUs.phone.value')}}</span>
+      </p>
+      <p class="item">
+        <span class="label">{{$t('contactUs.fax.label')}}：</span>
+        <span class="value">{{$t('contactUs.fax.value')}}</span>
+      </p>
+      <p class="item">
+        <span class="label">{{$t('contactUs.person.label')}}：</span>
+        <span class="value">{{$t('contactUs.person.value')}}</span>
+      </p>
+      <p class="item">
+        <span class="label">{{$t('contactUs.mail.label')}}：</span>
+        <span class="value">{{$t('contactUs.mail.value')}}</span>
+      </p>
+      <p class="item">
+        <span class="label">{{$t('contactUs.website.label')}}：</span>
+        <span class="value">{{$t('contactUs.website.value')}}</span>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -19,6 +53,20 @@
     font-size: 40px;
     text-align: center;
     margin-bottom: 50px;
+  }
+  .item_wrap{
+    .item{
+      padding-left: 300px;
+      font-size: 20px;
+      line-height: 1.8em;
+      .label{
+        width: 105px;
+        display: inline-block;
+      }
+      .value{
+        display: inline-block;
+      }
+    }
   }
 }
 </style>
